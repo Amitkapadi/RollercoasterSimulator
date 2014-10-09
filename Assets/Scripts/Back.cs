b69a08f9e66f556e5caf31311587de3e8d6b6039
andrewnakas <andrew.nakas@gmail.com>
2014-10-08 22:03:37 -0600

    Yes

---
+++ Assets/Scripts/Back.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Back : MonoBehaviour {
+	
+	public GUIStyle custom;
+	public GUIStyle custom1;
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	
+	void OnGUI () {
+		//GUILayout.BeginArea( new Rect( 0, 0, Screen.height*.5f, Screen.width*.5f) );
+		
+	
+		if (GUI.Button(new Rect(10,10, 100, 100),"Menu",custom1)){
+
+			Application.LoadLevel("menu");
+
+		}
+		
+	}
+}
+
